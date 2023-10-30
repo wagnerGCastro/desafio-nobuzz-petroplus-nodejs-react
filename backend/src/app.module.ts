3c0b7f0fadbc0a17a66088302098a38f5c7badb9
Wagner Castro <wagner_gcastro@hotmail.com>
2023-10-30 17:42:16 -0300

    #02 - Added cross-env package for ambient variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getEnvPath } from './utils/helpers';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // expandVariables: true,
+      envFilePath: getEnvPath(`${__dirname}/config/envs`),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
