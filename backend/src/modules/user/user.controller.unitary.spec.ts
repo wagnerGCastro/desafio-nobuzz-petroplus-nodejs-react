0249fdb0863e2be5b1788dffd354b26ad8718c82
Wagner Castro <wagner_gcastro@hotmail.com>
2023-11-01 13:13:16 -0300

    #08 - Created User Module without unit test

---
+++ backend/src/modules/user/user.controller.unitary.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let userController: UserController;
+  let userService: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(userController).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+});
