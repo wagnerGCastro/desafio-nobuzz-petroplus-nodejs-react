56036fdd577f9527c91f7fdc0c4f80964b956f8f
Wagner Castro <wagner_gcastro@hotmail.com>
2023-11-02 04:26:15 -0300

    #21 - Creating role for user
    - roles: admin, client
    - coment AuthGuard module user

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -5,7 +5,7 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@UseGuards(AuthGuard('jwt'))
+// @UseGuards(AuthGuard('jwt'))
 @Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
